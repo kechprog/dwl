28504823db9303223a5d8757fa4dcbd7c36d3712
Eduard <ed.kech2005@mail.ru>
2024-02-02 16:30:56 -0500

    charging status

--- bar/src/dbus_handles.cpp
+++ bar/src/dbus_handles.cpp
@@ -7,10 +7,47 @@
 #include <sys/poll.h>
 #include <utility>
 
-/*
- * TODO: charging should be done via another device, e.g. BATT <-> ACAD
- * this should also be configurable
- */
+
+static void dbus_query_bat(DBusConnection *conn, const char *property, void *ret)
+{
+	DBusError err;
+	const char* objPath = "/org/freedesktop/UPower/devices/battery_BATT";
+	const char* interfaceName = "org.freedesktop.DBus.Properties";
+
+	dbus_error_init(&err);
+
+	DBusMessage* msg = dbus_message_new_method_call(
+		"org.freedesktop.UPower", // service to contact
+		objPath,                  // object path
+		interfaceName,            // interface name
+		"Get");                   // method name
+
+	DBusMessageIter args;
+	dbus_message_iter_init_append(msg, &args);
+	const char* propInterface = "org.freedesktop.UPower.Device";
+	if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &propInterface)
+	||  !dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &property)) {
+	std::cerr << "Out of Memory!" << std::endl;
+	}
+
+	DBusMessage* reply = dbus_connection_send_with_reply_and_block(conn, msg, -1, &err);
+	if (!reply) {
+	std::cerr << "Failed to send message: " << (err.message ? err.message : "unknown error") << std::endl;
+	}
+
+	DBusMessageIter replyIter;
+	dbus_message_iter_init(reply, &replyIter);
+	DBusMessageIter variantIter;
+	if (dbus_message_iter_get_arg_type(&replyIter) == DBUS_TYPE_VARIANT) {
+		dbus_message_iter_recurse(&replyIter, &variantIter);
+		dbus_message_iter_get_basic(&variantIter, ret);
+	}
+
+	dbus_message_unref(reply);
+	dbus_message_unref(msg);
+}
+
+
 DbusListener::DbusListener(void)
 {
 	dbus_error_init(&err);
@@ -32,57 +69,32 @@ DbusListener::DbusListener(void)
 	make_initial_battery_state_request();
 }
 
+
 DbusListener::~DbusListener()
 {
 	/* todo */
 }
 
+
 void DbusListener::make_initial_battery_state_request()
 {
-// Replace this with the actual object path you're interested in
-    const char* objPath = "/org/freedesktop/UPower/devices/battery_BATT";
-    const char* interfaceName = "org.freedesktop.DBus.Properties";
-    const char* propertyName = "Percentage";
+	double bat_perc;
+	uint32_t bat_state;
+	dbus_query_bat(this->conn, "Percentage", &bat_perc);
+	dbus_query_bat(this->conn, "State", &bat_state);
 
-    DBusMessage* msg = dbus_message_new_method_call(
-        "org.freedesktop.UPower", // service to contact
-        objPath,                  // object path
-        interfaceName,            // interface name
-        "Get");                   // method name
-
-    DBusMessageIter args;
-    dbus_message_iter_init_append(msg, &args);
-    const char* propInterface = "org.freedesktop.UPower.Device";
-    if (!dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &propInterface)
-    ||  !dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &propertyName)) {
-        std::cerr << "Out of Memory!" << std::endl;
-    }
-
-    DBusMessage* reply = dbus_connection_send_with_reply_and_block(conn, msg, -1, &err);
-    if (!reply) {
-        std::cerr << "Failed to send message: " << (err.message ? err.message : "unknown error") << std::endl;
-    }
-
-    DBusMessageIter replyIter;
-    dbus_message_iter_init(reply, &replyIter);
-    DBusMessageIter variantIter;
-    if (dbus_message_iter_get_arg_type(&replyIter) == DBUS_TYPE_VARIANT) {
-        dbus_message_iter_recurse(&replyIter, &variantIter);
-        if (dbus_message_iter_get_arg_type(&variantIter) == DBUS_TYPE_DOUBLE) {
-            double percentage;
-            dbus_message_iter_get_basic(&variantIter, &percentage);
-		state::bat_percentage = percentage;
-        }
-    }
-
-    dbus_message_unref(reply);
-    dbus_message_unref(msg);
+	state::bat_percentage = bat_perc;
+	state::bat_is_charging = bat_state != 2;
+	std::cout << "Initial battery state: " << bat_perc << " " << bat_state << std::endl;
+	state::render();
 }
+
 
 int DbusListener::get_fd(void) const
 {
 	return fd;
 }
+
 
 void DbusListener::operator()(short int revents) const
 {
@@ -99,6 +111,7 @@ void DbusListener::operator()(short int revents) const
 		dbus_message_unref(msg);
 	}
 }
+
 
 std::pair<std::optional<double>, std::optional<bool>>
 DbusListener::parse_msg(DBusMessage *msg) const
@@ -134,6 +147,15 @@ DbusListener::parse_msg(DBusMessage *msg) const
 				dbus_message_iter_get_basic(&var, &val);
 				ret_perc = val;
 				std::cout << "Lowest level, battery: " << val << std::endl;
+			}
+
+			if (strcmp("State", key) == 0) {
+				dbus_message_iter_next(&entry);
+				DBusMessageIter var;
+				dbus_message_iter_recurse(&entry, &var);
+				uint32_t val;
+				dbus_message_iter_get_basic(&var, &val);
+				ret_status = val != 2; // is charging?
 			}
 
 			dbus_message_iter_next(&dict);
